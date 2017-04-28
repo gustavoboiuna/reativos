a975119adcd24ebc27ca82980f36e5d83c1f3e59
Gustavo Silva dos Santos <gustavosilva.nave@gmail.com>
2017-04-27 21:20:58 -0300

    Exercício 03
    Exercício 3, feito em sala de aula dia 26/04

---
+++ trabalhos/exercicio-03/Ex3.ino
@@ -0,0 +1,67 @@
+#include "xtea.c.h"
+
+#define LED 13
+#define BUT_PIN 2
+
+void setup () {
+    pinMode(LED, OUTPUT);
+    pinMode(BUT_PIN, INPUT);
+    Serial.begin(9600);
+}
+ 
+uint32_t key[] = { 1, 2, 3, 4};
+uint32_t v[]   = {10, 20, 30, 40, 50, 60, 70, 80, 90, 100, 110, 120, 130, 140, 150, 160};
+int ini, fim, len = 16;
+
+void loop () 
+{
+    static int led = 0;
+    digitalWrite(LED, led = !led);
+    int but = digitalRead(BUT_PIN);
+    ini = 0;
+    fim = 2;
+        
+    unsigned long t1 = micros();
+
+    int i;
+    for(i = ini; i < fim; i++) 
+    {
+      uint32_t new_v[] = {v[ini], v[ini +1]};
+
+      /*Serial.print("antes: ");
+        Serial.print(new_v[0]);
+        Serial.print(" ");
+        Serial.println(new_v[1]);*/
+        
+      encipher(32, new_v, key);
+      
+    /*Serial.print("durante: ");
+        Serial.print(new_v[0]);
+        Serial.print(" ");
+        Serial.println(new_v[1]);*/
+        
+      decipher(32, new_v, key);
+
+      /*Serial.print("depois: ");
+        Serial.print(new_v[0]);
+        Serial.print(" ");
+        Serial.println(new_v[1]);*/
+	  
+	  if(but == LOW)
+      {
+		digitalWrite(LED, HIGH);
+		while(1);
+	  }
+	  
+      ini = fim;
+      fim += 2;
+      
+      if(fim > len) 
+      {
+        break;
+      }
+    }
+
+    unsigned long t2 = micros();
+    Serial.println(t2-t1);
+}
